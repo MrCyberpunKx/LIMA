88ea8d9bf0c0c3e807c3b1afab91f12578ab29a1
Daniel Santos "Frank Deckard" Lopes Silva <71890228+MrCyberpunKx@users.noreply.github.com>
2020-10-09 13:47:40 -0300

    Add files via upload

---
+++ Aula09_09_10_2020/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula09_09_10_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
